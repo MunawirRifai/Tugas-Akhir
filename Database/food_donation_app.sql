f9e3778db261bee19847253d7a03d742739ba9d0
MunawirRifai <munawirr90@gmail.com>
2026-05-22 19:22:26 +0700

    clean database

--- Database/food_donation_app.sql
+++ Database/food_donation_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 16, 2026 at 10:53 AM
+-- Generation Time: May 22, 2026 at 12:20 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -45,20 +45,6 @@ CREATE TABLE `foods` (
   `original_quantity` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `foods`
---
-
-INSERT INTO `foods` (`id`, `user_id`, `food_name`, `description`, `quantity`, `latitude`, `longitude`, `address`, `photo_url`, `expired_at`, `created_at`, `status`, `claimed_by`, `claimed_quantity`, `original_quantity`) VALUES
-(46, 33, 'martabak', 'martabak', 0, -6.966590784246315, 107.63754451764814, '-6.966590784246315, 107.63754451764814', 'http://localhost:8080/uploads/foods/c0aff152-5199-4126-b26a-97a718e6e28c_martabak manis.jpg', '2026-05-16 13:58:52', '2026-05-16 10:59:11', 'PICKED_UP', 34, 2, 3),
-(47, 33, 'martabak', 'martabak', 0, -6.96829472123741, 107.63728702558264, '-6.96829472123741, 107.63728702558264', 'http://localhost:8080/uploads/foods/7f2a4d90-ba36-4f17-b61c-79c3a8c069a3_martabak manis.jpg', '2026-05-16 14:02:09', '2026-05-16 11:02:27', 'PICKED_UP', 34, 1, 5),
-(48, 34, 'martabak', 'martabak', 1, -6.966686630541428, 107.63767111778259, '-6.966686630541428, 107.63767111778259', 'http://localhost:8080/uploads/foods/e4b6b0b6-9944-428e-85a1-513125fc54c9_martabak manis.jpg', '2026-05-16 14:29:33', '2026-05-16 11:29:55', 'CANCELED', 33, 2, 3),
-(49, 34, 'martabak', 'martabak', 0, -6.96951942179929, 107.63696301460267, '-6.96951942179929, 107.63696301460267', 'http://localhost:8080/uploads/foods/a3609f4c-3a0c-48be-a510-2bf11bde0d6a_martabak manis.jpg', '2026-05-16 16:48:36', '2026-05-16 13:48:52', 'PICKED_UP', 33, 1, 3),
-(50, 34, 'Martabak', 'Martabak', 0, -6.967772890866756, 107.63443100929261, '-6.967772890866756, 107.63443100929261', 'http://localhost:8080/uploads/foods/c660d430-eb09-49c9-a1f3-767aa6d17a25_martabak manis.jpg', '2026-05-16 17:32:36', '2026-05-16 14:32:58', 'PICKED_UP', 33, 1, 3),
-(51, 34, 'martabak', 'martabak', 0, -6.965834659897938, 107.63760674476625, '-6.965834659897938, 107.63760674476625', 'http://localhost:8080/uploads/foods/9758678f-4047-45b4-a9de-6b3720565693_martabak manis.jpg', '2026-05-16 17:52:24', '2026-05-16 14:52:48', 'PICKED_UP', 33, 1, 3),
-(52, 34, 'martabak', 'martabak', 0, -6.966175448341091, 107.63769257545471, '-6.966175448341091, 107.63769257545471', 'http://localhost:8080/uploads/foods/3ffe7281-5475-46ef-aa3e-94154918baef_martabak manis.jpg', '2026-05-16 17:56:16', '2026-05-16 14:56:34', 'PICKED_UP', 33, 1, 3),
-(53, 34, 'martabak', 'martabak', 0, -6.976505480431825, 107.6350961971283, '-6.976505480431825, 107.6350961971283', 'http://localhost:8080/uploads/foods/3f198a38-49bc-4237-b2ef-a49a206014c6_martabak manis.jpg', '2026-05-16 17:59:02', '2026-05-16 14:59:18', 'PICKED_UP', 10, 2, 5);
-
 -- --------------------------------------------------------
 
 --
@@ -86,7 +72,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `created_at`, `email`, `full_name`, `is_verified`, `password_hash`, `phone`, `photo_url`, `verification_code`, `verification_expired_at`, `is_banned`, `role`, `timeout_until`) VALUES
-(10, '2026-04-09 08:05:26.864518', 'munawirr90@gmail.com', 'Munawir Rifa\'i', b'1', '$2y$10$YJiBSKNsHFsYtjM.VJYsp.8nOAAbuVFZ20SCelP8DRupEj395oAeW', '0822222222', 'http://localhost:8080/uploads/profile/1776512594302_IMG-20230925-WA0000.jpg', NULL, NULL, NULL, NULL, NULL),
+(10, '2026-04-09 08:05:26.864518', 'munawirr90@gmail.com', 'Munawir Rifa\'i', b'1', '$2y$10$3zLgcGT/DsBRtPXtPvdMuuo.GPXt4PqOmWOYMuE69ZLtAiU/tHtBO', '0822222222', 'http://localhost:8080/uploads/profile/1776512594302_IMG-20230925-WA0000.jpg', NULL, NULL, NULL, NULL, NULL),
 (12, '2026-04-19 15:57:00.736582', 'gading@gmail.com', 'Gading mustiko', b'1', '$2a$10$kKdonjj8LOS.X6F8IeX2DOgbIK.hSQmfpAStOY.2n3B7pBO6G/V4O', '0800000000', 'http://localhost:8080/uploads/profile/0e7b61f2-10f1-46a4-abbc-b38d91f58637_gading.jpeg', NULL, NULL, NULL, NULL, NULL),
 (33, '2026-05-15 18:19:33.569005', 'munawirr158@gmail.com', 'Munawir Rifa\'i', b'1', '$2a$10$Qt3iFSfBawavnxIogSTPJ.Ji2t8vvVIH6.bsBqaInDVOPBPBg3Gbi', '0888888888', NULL, NULL, NULL, NULL, NULL, NULL),
 (34, '2026-05-15 18:32:23.660185', 'zil42231@gmail.com', 'Gading', b'1', '$2a$10$3wiSHmoCg/QlAEdLvz.xtePp5AQM9hgpD9/.ug3aofoTjiWY7uERy', '0877777777', NULL, NULL, NULL, NULL, NULL, NULL);
@@ -118,7 +104,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `foods`
 --
 ALTER TABLE `foods`
-  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=620;
 
 --
 -- AUTO_INCREMENT for table `users`
